e622d7af410a68d0bf0ffbfddbb0e09b0c729f73
dan13ram <dan13ram@gmail.com>
2026-04-16 14:58:34 +0530

    feat: added deploy script + fixed test for MerkleReserveMinter

---
+++ script/DeployMerkleReserveMinter.s.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
+
+import { MerkleReserveMinter } from "../src/minters/MerkleReserveMinter.sol";
+
+contract DeployContracts is Script {
+    using Strings for uint256;
+
+    string configFile;
+
+    function _getKey(string memory key) internal view returns (address result) {
+        (result) = abi.decode(vm.parseJson(configFile, string.concat(".", key)), (address));
+    }
+
+    function run() public {
+        uint256 chainID = vm.envUint("CHAIN_ID");
+        uint256 key = vm.envUint("PRIVATE_KEY");
+
+        configFile = vm.readFile(string.concat("./addresses/", Strings.toString(chainID), ".json"));
+
+        address deployerAddress = vm.addr(key);
+        address managerAddress = _getKey("Manager");
+        address protocolRewards = _getKey("ProtocolRewards");
+
+        console2.log("~~~~~~~~~~ CHAIN ID ~~~~~~~~~~~");
+        console2.log(chainID);
+
+        console2.log("~~~~~~~~~~ DEPLOYER ~~~~~~~~~~~");
+        console2.log(deployerAddress);
+
+        console2.log("~~~~~~~~~~ MANAGER ~~~~~~~~~~~");
+        console2.log(managerAddress);
+
+        console2.log("~~~~~~~~~~ PROTOCOL REWARDS ~~~~~~~~~~~");
+        console2.log(protocolRewards);
+
+        vm.startBroadcast(deployerAddress);
+
+        address merkleReserveMinter = address(new MerkleReserveMinter(managerAddress, protocolRewards));
+
+        vm.stopBroadcast();
+
+        string memory filePath = string(abi.encodePacked("deploys/", chainID.toString(), ".merkle_reserve_minter.txt"));
+
+        vm.writeFile(filePath, "");
+        vm.writeLine(filePath, string(abi.encodePacked("Merkle Reserve Minter: ", addressToString(merkleReserveMinter))));
+
+        console2.log("~~~~~~~~~~ MERKLE RESERVE MINTER ~~~~~~~~~~~");
+        console2.logAddress(merkleReserveMinter);
+    }
+
+    function addressToString(address _addr) private pure returns (string memory) {
+        bytes memory s = new bytes(40);
+        for (uint256 i = 0; i < 20; i++) {
+            bytes1 b = bytes1(uint8(uint256(uint160(_addr)) / (2 ** (8 * (19 - i)))));
+            bytes1 hi = bytes1(uint8(b) / 16);
+            bytes1 lo = bytes1(uint8(b) - 16 * uint8(hi));
+            s[2 * i] = char(hi);
+            s[2 * i + 1] = char(lo);
+        }
+        return string(abi.encodePacked("0x", string(s)));
+    }
+
+    function char(bytes1 b) private pure returns (bytes1 c) {
+        if (uint8(b) < 10) return bytes1(uint8(b) + 0x30);
+        else return bytes1(uint8(b) + 0x57);
+    }
+}
