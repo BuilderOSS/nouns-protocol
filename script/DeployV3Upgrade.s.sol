722e7302062ce6aba9d24140c3ea8ff23e99de1e
dan13ram <dan13ram@gmail.com>
2026-06-02 13:26:53 +0530

    feat: deployed v3.0.0 for eth sepolia

--- script/DeployV3Upgrade.s.sol
+++ script/DeployV3Upgrade.s.sol
@@ -8,7 +8,7 @@ import { IManager } from "../src/manager/IManager.sol";
 import { Manager } from "../src/manager/Manager.sol";
 import { Governor } from "../src/governance/governor/Governor.sol";
 
-contract DeployGovernorV3 is Script {
+contract DeployV3Upgrade is Script {
     using Strings for uint256;
 
     string configFile;
@@ -19,37 +19,78 @@ contract DeployGovernorV3 is Script {
 
     function run() public {
         uint256 chainID = block.chainid;
-        uint256 key = vm.envUint("PRIVATE_KEY");
 
         configFile = vm.readFile(string.concat("./addresses/", Strings.toString(chainID), ".json"));
 
-        address deployerAddress = vm.addr(key);
-        address managerProxy = _getKey("Manager");
+        address deployerAddress = vm.addr(vm.envUint("PRIVATE_KEY"));
+        IManager managerProxy = IManager(_getKey("Manager"));
+        address oldManagerImpl = _getKey("ManagerImpl");
         address oldGovernorImpl = _getKey("Governor");
+        address auctionImpl = _getKey("Auction");
+        address treasuryImpl = _getKey("Treasury");
+        address tokenImpl = _getKey("Token");
+        address metadataRendererImpl = _getKey("MetadataRenderer");
+        address builderRewardsRecipient = _getKey("BuilderRewardsRecipient");
 
+        _deployUpgrade(
+            deployerAddress,
+            managerProxy,
+            oldManagerImpl,
+            oldGovernorImpl,
+            auctionImpl,
+            treasuryImpl,
+            tokenImpl,
+            metadataRendererImpl,
+            builderRewardsRecipient,
+            chainID
+        );
+    }
+
+    function _deployUpgrade(
+        address deployerAddress,
+        IManager managerProxy,
+        address oldManagerImpl,
+        address oldGovernorImpl,
+        address auctionImpl,
+        address treasuryImpl,
+        address tokenImpl,
+        address metadataRendererImpl,
+        address builderRewardsRecipient,
+        uint256 chainID
+    ) private {
         console2.log("~~~~~~~~~~ CHAIN ID ~~~~~~~~~~~");
         console2.log(chainID);
         console2.log("~~~~~~~~~~ DEPLOYER ~~~~~~~~~~~");
         console2.log(deployerAddress);
         console2.log("~~~~~~~~~~ MANAGER PROXY ~~~~~~~~~~~");
-        console2.logAddress(managerProxy);
+        console2.logAddress(address(managerProxy));
         console2.log("~~~~~~~~~~ OLD GOVERNOR IMPL ~~~~~~~~~~~");
         console2.logAddress(oldGovernorImpl);
+        console2.log("~~~~~~~~~~ OLD MANAGER IMPL ~~~~~~~~~~~");
+        console2.logAddress(oldManagerImpl);
 
         vm.startBroadcast(deployerAddress);
 
-        address newGovernorImpl = address(new Governor(managerProxy));
+        address newGovernorImpl = address(new Governor(address(managerProxy)));
+        address newManagerImpl =
+            address(new Manager(tokenImpl, metadataRendererImpl, auctionImpl, treasuryImpl, newGovernorImpl, builderRewardsRecipient));
+
+        managerProxy.upgradeTo(newManagerImpl);
 
         vm.stopBroadcast();
 
-        string memory filePath = string(abi.encodePacked("deploys/", chainID.toString(), ".version3_governor.txt"));
+        string memory filePath = string(abi.encodePacked("deploys/", chainID.toString(), ".version3_upgrade.txt"));
 
         vm.writeFile(filePath, "");
         vm.writeLine(filePath, string(abi.encodePacked("Old Governor implementation: ", addressToString(oldGovernorImpl))));
         vm.writeLine(filePath, string(abi.encodePacked("New Governor implementation: ", addressToString(newGovernorImpl))));
+        vm.writeLine(filePath, string(abi.encodePacked("Old Manager implementation: ", addressToString(oldManagerImpl))));
+        vm.writeLine(filePath, string(abi.encodePacked("New Manager implementation: ", addressToString(newManagerImpl))));
 
         console2.log("~~~~~~~~~~ NEW GOVERNOR IMPL ~~~~~~~~~~~");
         console2.logAddress(newGovernorImpl);
+        console2.log("~~~~~~~~~~ NEW MANAGER IMPL ~~~~~~~~~~~");
+        console2.logAddress(newManagerImpl);
     }
 
     function addressToString(address _addr) private pure returns (string memory) {
